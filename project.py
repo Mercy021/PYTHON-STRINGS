6372bc87233067141e04b157f6195b77b2b45fa4
mercy021 <kinyanjuimercywangari@gmail.com>
2021-04-01 02:05:46 +0300

    first commit

---
+++ project.py
@@ -0,0 +1,44 @@
+#prints class tuple1
+guitar1=("Acoustic","Classic","Electric","Semi-acoustic")
+print(guitar1)
+#prints class tuple2
+guitar2=("body","fret","bridge","head")
+print(guitar2)
+#we use index operator []to access an item in a tuple
+#index starts from 0
+print(guitar1[0])
+print(guitar1[3])
+print(guitar2[2])
+print(guitar2[3])
+#prints out the length of tup1,tup2
+print(len(guitar1))
+print(len(guitar2))
+#membership test in tuple
+z=("ama","ala","yala","mara")
+print("ama"in z)
+print("yala"in z)
+print("ala"in z)
+print("mara"not in z)
+print("yazi"in z)
+#using a for loop to iterate a tuple
+browsers=("firefox","microsoft edge","safari","chrome")
+for p in browsers:
+    print(p)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
